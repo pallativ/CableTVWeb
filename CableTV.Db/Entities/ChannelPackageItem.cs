b5bda3cc0ca1e5a27bb0cb33c8cfd18b1bc52a8c
Veera <pkondalu@gmail.com>
2021-07-29 11:08:06 +0530

    Adding Id generation code

--- CableTV.Db/Entities/ChannelPackageItem.cs
+++ CableTV.Db/Entities/ChannelPackageItem.cs
@@ -1,9 +1,15 @@
-﻿namespace CableTV.Db
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CableTV.Db
 {
     public class ChannelPackageItem
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public int ChannelPackageId { get; set; }
+        [Required]
         public int ChannelId { get; set; }
         public ChannelPackage Package { get; set; }
         public Channel Channel { get; set; }
